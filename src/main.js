ba67f0ec3eac24b6c1466c7f5aec3bb722d63b26
zengxianlin <877529974@qq.com>
2016-12-01 18:32:07 +0800

    修改man.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+/*
+* @Author: zengxianlin
+* @Date:   2016-12-01 18:29:57
+* @Last Modified by:   877529974@qq.com
+* @Last Modified time: 2016-12-01 18:30:08
+*/
+'use strict';
 import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource'
